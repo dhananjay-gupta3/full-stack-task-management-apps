54b7f85907dd46791e22f4cd606505a7f71fa244
dhananjay-gupta3 <102008241+dhananjay-gupta3@users.noreply.github.com>
2025-01-27 10:20:28 +0530

    Update

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,22 +1,3 @@
-// import mongoose from "mongoose";
-
-
-// export const connectDB = async () => {
-//     try {
-//         await mongoose.connect(process.env.MONGO_URL, {
-//             useNewUrlParser: true,
-//             useUnifiedTopology: true,
-//             connectTimeoutMS: 30000, // 30 seconds
-//             socketTimeoutMS: 30000, // 30 seconds
-
-//         });
-//         console.log("Database connected successfully");
-//     } catch (err) {
-//         console.log(err)
-//     }
-// }
-
-
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 
